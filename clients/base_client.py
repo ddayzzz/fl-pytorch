fix memory leak(by changing the model broadcast mechanism)

clients/base_client.py
@@ -4,26 +4,13 @@ from torch.utils.data import DataLoader, Dataset
 from torch import optim
 from torch import nn
 from utils.metrics import Meter
-from typing import Dict, Union
-
-
-def get_model_parameters_dict(model: nn.Module) -> Dict[str, torch.Tensor]:
-    result = dict()
-    for k, v in model.named_parameters():
-        result[k] = v.detach().clone()
-    return result
-
-
-def set_model_parameters_dict(model: nn.Module, parameters_dict: Dict[str, torch.Tensor]):
-    # torch 自带 copy 的方法
-    for k, v in model.named_parameters():
-        # 见数据复制到 model 的位置, 二者是不共享位置的
-        v.data.copy_(parameters_dict[k])
+from typing import Dict, Union, Tuple
+import copy
 
 
 class BaseClient(object):
 
-    def __init__(self, id, model: nn.Module, dataset: Dataset, dataset_type: str, options: dict):
+    def __init__(self, id, dataset: Dataset, dataset_type: str, options: dict):
         """
 
         :param id:
@@ -38,13 +25,13 @@ class BaseClient(object):
         # 这个必须是客户端相关的
         self.num_dataset = len(dataset)
         self.num_workers = options['num_workers']
-        self.batch_size = options['batch_size']
+        self.batch_size = options['train_batch_size'] if dataset_type == 'train' else options['test_batch_size']
         self.device = options['device']
-        self.model = model
         self.dataset_type = dataset_type
         #
         self.criterion = self.create_criterion()
-        self.optimizer = self.create_optimizer()
+        # TODO, 待验证, 每个客户端创建一个 optimizer 的对象可能会造成内存泄露!!
+        # self.optimizer = self.create_optimizer()
         #
         self.dataset_loader = self.create_data_loader(dataset)
 
@@ -58,27 +45,21 @@ class BaseClient(object):
     def create_criterion(self):
         return nn.CrossEntropyLoss(reduction='mean').to(self.device)
 
-    def create_optimizer(self):
+    def create_optimizer(self, model: nn.Module):
         if self.dataset_type == 'train':
-            opt = optim.SGD(self.model.parameters(), lr=self.options['lr'], momentum=self.options['momentum'],
+            opt = optim.SGD(model.parameters(), lr=self.options['lr'], momentum=self.options['momentum'],
                             weight_decay=self.options['wd'])
             return opt
         else:
             return None
-
-    def get_model_parameters_dict(self) -> Dict[str, torch.Tensor]:
-        return get_model_parameters_dict(self.model)
-
-    def set_model_parameters_dict(self, parameters_dict: Dict[str, torch.Tensor]):
-        set_model_parameters_dict(self.model, parameters_dict)
 
     def count_correct(self, pred, targets):
         _, predicted = torch.max(pred, 1)
         correct = predicted.eq(targets).sum()
         return correct
 
-    def test(self) -> Dict[str, Union[int, Meter]]:
-        self.model.eval()
+    def test(self, model: nn.Module) -> Dict[str, Union[int, Meter]]:
+        model.eval()
 
         loss_meter = Meter()
         acc_meter = Meter()
@@ -88,7 +69,7 @@ class BaseClient(object):
 
             for batch_idx, (X, y) in enumerate(self.dataset_loader):
                 X, y = X.to(self.device), y.to(self.device)
-                pred = self.model(X)
+                pred = model(X)
                 loss = self.criterion(pred, y)
                 correct = self.count_correct(pred, y)
                 #
@@ -103,11 +84,12 @@ class BaseClient(object):
             'num_samples': num_all_samples
         }
 
-    def solve_epochs(self, round_i, num_epochs, hide_output: bool = False) -> Dict[str, Union[int, Meter]]:
+    def solve_epochs(self, round_i, model: nn.Module, num_epochs, hide_output: bool = False) -> Tuple[Dict[str, Union[int, Meter]], Dict[str, torch.Tensor]] :
         loss_meter = Meter()
         acc_meter = Meter()
         num_all_samples = 0
-        self.model.train()
+        optimizer = self.create_optimizer(model)
+        model.train()
 
         with tqdm.trange(num_epochs, disable=hide_output) as t:
 
@@ -117,13 +99,13 @@ class BaseClient(object):
                     # from IPython import embed
                     X, y = X.to(self.device), y.to(self.device)
 
-                    self.optimizer.zero_grad()
-                    pred = self.model(X)
+                    optimizer.zero_grad()
+                    pred = model(X)
 
                     loss = self.criterion(pred, y)
                     loss.backward()
                     # torch.nn.utils.clip_grad_norm(self.model.parameters(), 60)
-                    self.optimizer.step()
+                    optimizer.step()
 
                     correct_sum = self.count_correct(pred, y)
                     num_samples = y.size(0)
@@ -133,10 +115,11 @@ class BaseClient(object):
                     if (batch_idx % 10 == 0):
                         # 纯数值, 这里使用平均的损失
                         t.set_postfix(mean_loss=loss.item())
-
+        # 返回参数
+        state_dict = model.cpu().state_dict()
         # 输出相关的参数
         return {
             'loss_meter': loss_meter,
             'acc_meter': acc_meter,
             'num_samples': num_all_samples
-        }
+        }, state_dict

trainers/fedavg.py
@@ -7,7 +7,8 @@ class FedAvg(FedBase):
         super(FedAvg, self).__init__(options=options, model=model, dataset_info=dataset_info, append2metric=None)
 
     def aggregate(self, solns, num_samples):
-        return self.aggregate_parameters_weighted(solns, num_samples)
+        new_state = self.aggregate_parameters_weighted(solns, num_samples)
+        self.global_model.load_state_dict(new_state)
 
     def train(self):
         for round_i in range(self.num_rounds):
@@ -16,9 +17,7 @@ class FedAvg(FedBase):
             selected_clients = self.select_clients(round_i=round_i, clients_per_rounds=self.clients_per_round)
 
             solns, num_samples = self.solve_epochs(round_i, clients=selected_clients)
-
-
-            self.latest_model = self.aggregate(solns, num_samples)
+            self.aggregate(solns, num_samples)
             # eval on test
             if round_i % self.eval_on_test_every_round == 0:
                 self.eval_on(round_i=round_i, clients=self.test_clients, client_type='test')
@@ -30,4 +29,7 @@ class FedAvg(FedBase):
                 # self.save_model(round_i)
                 self.metrics.write()
 
-        self.metrics.write()
+            # if round_i % 50 == 0:
+            #     torch.cuda.empty_cache()
+
+        # self.metrics.write()

trainers/fedbase.py
@@ -8,11 +8,11 @@ import torch
 from torch.utils.data import Dataset
 from torch import nn, optim
 import pandas as pd
-from clients.base_client import BaseClient, get_model_parameters_dict, set_model_parameters_dict
+from clients.base_client import BaseClient
 from utils.metrics import Metrics
 from utils.flops_counter import get_model_complexity_info
 from dataset.read_data import DatasetInfo
-
+import copy
 
 
 class FedBase(abc.ABC):
@@ -47,11 +47,10 @@ class FedBase(abc.ABC):
         self.eval_on_test_every_round = options['eval_on_test_every']
         self.eval_on_validation_every_round = options['eval_on_validation_every']
         # 使用 client 的API
-        self.latest_model = self.get_latest_model()
+        self.global_model = model
         self.name = '_'.join(['', f'wn[{options["clients_per_round"]}]', f'num_train[{self.num_train_clients}]', f'num_test[{self.num_test_clients}]'])
         self.metrics = Metrics(options=options, name=self.name, append2suffix=append2metric, result_prefix=options['result_prefix'])
         self.quiet = options['quiet']
-
 
     def setup_model(self, model):
         dev = self.options['device']
@@ -63,10 +62,6 @@ class FedBase(abc.ABC):
         # return model, flops, params_num, model_bytes
         return model, None, None, None
 
-    def get_latest_model(self) -> Dict[str, torch.Tensor]:
-        # 默认使用第一个训练的客户端的
-        return get_model_parameters_dict(self.model)
-
     def create_clients_group(self, users: Iterable[Any], train_or_test_dataset_obj: Dict[Any, Dataset], dataset_type) -> OrderedDict[Any, BaseClient]:
         """
 
@@ -75,7 +70,7 @@ class FedBase(abc.ABC):
         """
         all_clients = collections.OrderedDict()
         for user in users:
-            c = BaseClient(id=user, model=self.model, dataset=train_or_test_dataset_obj[user], dataset_type=dataset_type, options=self.options)
+            c = BaseClient(id=user, dataset=train_or_test_dataset_obj[user], dataset_type=dataset_type, options=self.options)
             all_clients[user] = c
         return all_clients
 
@@ -95,9 +90,6 @@ class FedBase(abc.ABC):
             return result
         else:
             raise NotImplementedError
-
-    def set_latest_model(self):
-        set_model_parameters_dict(self.model, self.latest_model)
 
     @abc.abstractmethod
     def aggregate(self, *args, **kwargs):
@@ -119,21 +111,21 @@ class FedBase(abc.ABC):
 
     def aggregate_parameters_weighted(self, solns: List[Dict[str, torch.Tensor]], num_samples: List[int]) -> Dict[str, torch.Tensor]:
         """
-        聚合模型
-        :param solns: 列表.
-        :param kwargs:
-        :return: 聚合后的参数
+        加权聚合模型
+        :param solns: 这些参数保存在CPU中
+        :param num_samples:
+        :return:
         """
-        lastes = dict()
+        # 产生新的数据
+        factors = np.asarray(num_samples) / sum(num_samples)
+        result = dict()
         for p_name in solns[0].keys():
             new = torch.zeros_like(solns[0][p_name])
-            sz = 0
-            for num_sample, sol in zip(num_samples, solns):
-                new += sol[p_name] * num_sample
-                sz += num_sample
-            new /= sz
-            lastes[p_name] = new
-        return lastes
+            for factor, sol in zip(factors, solns):
+                # inplace, factor * 当前参数
+                new.add_(sol[p_name], alpha=factor)
+            result[p_name] = new
+        return result
 
     def eval_on(self, round_i, clients: Iterable[BaseClient], client_type):
         df = pd.DataFrame(columns=['client_id', 'mean_acc', 'mean_loss', 'num_samples'])
@@ -143,8 +135,7 @@ class FedBase(abc.ABC):
         correct_num = []
         for c in clients:
             # 设置网络
-            self.set_latest_model()
-            stats = c.test()
+            stats = c.test(self.global_model)
 
             num_samples.append(stats['num_samples'])
             losses.append(stats['loss_meter'].sum)
@@ -173,16 +164,15 @@ class FedBase(abc.ABC):
 
         solns = []
         for c in clients:
-            self.set_latest_model()
+            model_to_client = copy.deepcopy(self.global_model).to(self.device)
             # 保存信息
-            stat = c.solve_epochs(round_i, num_epochs, hide_output=self.quiet)
+            stat, state_dict = c.solve_epochs(round_i, model_to_client, num_epochs, hide_output=self.quiet)
 
             num_samples.append(stat['num_samples'])
             losses.append(stat['loss_meter'].sum)
             correct_num.append(stat['acc_meter'].sum)
             #
-            soln = c.get_model_parameters_dict()
-            solns.append(soln)
+            solns.append(state_dict)
             # 写入测试的相关信息
             # self.metrics.update_commu_stats(round_i, flop_stat)
 

trainers/fedprox.py
@@ -82,20 +82,22 @@ class FedProx(FedBase):
         :param kwargs:
         :return: 聚合后的参数
         """
-        lastes = list()
+        factors = np.asarray(num_samples) / sum(num_samples)
+        result = dict()
         num_params = len(solns[0])
         for p_name in range(num_params):
             new = torch.zeros_like(solns[0][p_name])
-            sz = 0
-            for num_sample, sol in zip(num_samples, solns):
-                new += sol[p_name] * num_sample
-                sz += num_sample
-            new /= sz
-            lastes.append(new)
-        return lastes
+            for factor, sol in zip(factors, solns):
+                new.add_(sol[p_name], alpha=factor)
+            result.append(new)
+        return result
 
     def aggregate(self, solns, num_samples):
-        return self.aggregate_parameters_weighted(solns, num_samples)
+        # return self.aggregate_parameters_weighted(solns, num_samples)
+        result = self.aggregate_parameters_weighted(solns, num_samples)
+        del solns
+        del self.latest_model
+        return result
 
     def solve_epochs(self,round_i, clients: List[int], num_epochs=None):
         if num_epochs is None: